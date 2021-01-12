3f6829fdfc434387582088f530d41e7aa1061bb3
yoshinyan <hai_hai_hai_1229@yahoo.co.jp>
2021-01-13 00:00:15 +0900

    編集できるようにした

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -25,6 +25,39 @@ class AttendancesController < ApplicationController
     redirect_to attendances_index_path
   end
 
+  def edit
+    user_id = 1
+    @attendance = Attendance.find_by(user_id: user_id, date: params[:date])
+  end
+
+  def update
+    user_id = 1
+    @attendance = Attendance.find_by(user_id: user_id, date: params["attendance"][:date])
+
+    start_time_at = Time.zone.local(
+      params["attendance"]["start_time_at(1i)"],
+      params["attendance"]["start_time_at(2i)"],
+      params["attendance"]["start_time_at(3i)"],
+      params["attendance"]["start_time_at(4i)"],
+      params["attendance"]["start_time_at(5i)"]
+    )
+
+    end_time_at = Time.zone.local(
+      params["attendance"]["end_time_at(1i)"],
+      params["attendance"]["end_time_at(2i)"],
+      params["attendance"]["end_time_at(3i)"],
+      params["attendance"]["end_time_at(4i)"],
+      params["attendance"]["end_time_at(5i)"]
+    )
+
+    @attendance.start_time_at = start_time_at
+    @attendance.end_time_at = end_time_at
+    @attendance.note = params["attendance"]["note"]
+    @attendance.save!
+
+    redirect_to attendances_index_path
+  end
+
   def delete
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   get 'attendances/show'
   post 'attendances/start_time'
   post 'attendances/end_time'
-  post 'attendances/edit'
+  get 'attendances/edit'
+  patch 'attendances/update'
+
   get 'attendances/delete'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
